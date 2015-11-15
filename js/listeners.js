72daa12117de63c9e2221c964aafe69ffa94fa13
William Jacobson <williamjacobson@Williams-MacBook-Pro.local>
2015-11-15 17:12:50 -0500

    yessir

--- js/listeners.js
+++ js/listeners.js
@@ -1,15 +1,14 @@
 // This file is where we handle all the motion responses
 
-// Start all loop audios playing (loopers is defined in soundcloud.js)
-
+// Start all loop audios playing (they are muted by default)
 var loopers = document.getElementsByClassName('loop');
-
 setTimeout(function() {
 	for (var i = 0; i < loopers.length; i++) {
 		loopers[i].play();
 	}
 }, 2000);
 
+// These are some nice variables that we use in our logic below
 var fresh = true;
 var oneDisabled = false;
 var twoDisabled = false;
@@ -35,7 +34,7 @@ var isPlaying = [];
 setTimeout(registerListeners, 3000);
 
 // If it's muted, turn muted on and push into 'isPlaying' memory. Otherwise, mute it and remove it from 'isPlaying'
-function playLoop(element, id) {
+function playLoop(element) {
 	if (fresh) {
 		for (var i = 0; i < loopers.length; i++) {
 			loopers[i].play();
@@ -48,7 +47,7 @@ function playLoop(element, id) {
 	if (isPlaying.indexOf(audio) < 0) {
 		audio.muted = false;
 		isPlaying.push(audio);
-		$('#'+ id).css('borderColor', '#6131bc');
+		$('#'+ element.id).css('borderColor', '#6131bc');
 	} else {
 		audio.muted = true;
 		var index = isPlaying.indexOf(audio);
@@ -60,11 +59,11 @@ function playLoop(element, id) {
 			}
 			fresh = true;
 		}
-		$('#'+ id).css('borderColor', '#fff');
+		$('#'+ element.id).css('borderColor', '#fff');
 	}
 }
 
-// Go back to time 0 and play. Used for nonlooping audio elements with fixed playbackRate. Used in motion event handlers.
+// Go back to time 0 and play. Used for nonlooping audio elements with fixed playbackRate. 
 function playOnce(element) {
 	var audio = element.getElementsByTagName('audio')[0];
 	audio.currentTime = 0;
@@ -74,15 +73,13 @@ function playOnce(element) {
 // Mutes / unmutes all audios that were playing
 function pauseAll() {
 	if (!allPaused) {
-		for (var i = 0; i < isPlaying.length; i++) {
-			isPlaying[i].muted = true; 
-			isPlaying[i].pause();
+		for (var i = 0; i < loopers.length; i++) { 
+			loopers[i].pause();
 		}
 		$('#pause').css('borderColor', '#b71f1f');
 	} else {
-		for (var j = 0; j < isPlaying.length; j++) {
-			isPlaying[j].muted = false;
-			isPlaying[j].play();
+		for (var j = 0; j < loopers.length; j++) {
+			loopers[j].play();
 		}
 		$('#pause').css('borderColor', '#fff');
 	}
@@ -102,141 +99,83 @@ function registerListeners(){
 		}, 230);
 	});
 
+	function sendToLoop(element, theRightOne) {
+		if (window[theRightOne]) return;
+		window[theRightOne] = true;
+		playLoop(element);
+		setTimeout(function() {
+			window[theRightOne] = false;
+		}, 500);
+	}
+
+	function sendToPlayOnce(element, theRightOne) {
+		if (window[theRightOne]) return;
+		window[theRightOne] = true;
+		playOnce(element);
+		setTimeout(function() {
+			window[theRightOne] = false;
+		}, 200);
+	}
+
 	// examples for id usage (can use a class as well, if want multiple elements to respond)
 	$('#one').on('motion', function(){
-		if (oneDisabled) return;
-		oneDisabled = true;
-		playLoop(this, 'one');
-		setTimeout(function() {
-			oneDisabled = false;
-			// 
-		}, 500);
+		sendToLoop(this, 'oneDisabled');
 	});
 
 	$('#two').on('motion', function(){
-		if (twoDisabled) return;
-		twoDisabled = true;
-		playLoop(this, 'two');
-		setTimeout(function() {
-			twoDisabled = false;
-		}, 500);
+		sendToLoop(this, 'twoDisabled');
 	});
 
 	$('#three').on('motion', function(){
-		if (threeDisabled) return;
-		threeDisabled = true;
-		playLoop(this, 'three');
-		setTimeout(function() {
-			threeDisabled = false;
-		}, 500);
+		sendToLoop(this, 'threeDisabled');
 	});
 
 	$('#four').on('motion', function(){
-		if (fourDisabled) return;
-		fourDisabled = true;
-		playLoop(this, 'four');
-		setTimeout(function() {
-			fourDisabled = false;
-		}, 500);
+		sendToLoop(this, 'fourDisabled');
 	});
 
 	$('#five').on('motion', function(){
-		if (fiveDisabled) return;
-		fiveDisabled = true;
-		playLoop(this, 'five');
-		setTimeout(function() {
-			fiveDisabled = false;
-		}, 500);
+		sendToLoop(this, 'fiveDisabled');
 	});
 
 	$('#six').on('motion', function(){
-		if (sixDisabled) return;
-		sixDisabled = true;
-		playLoop(this, 'six');
-		setTimeout(function() {
-			sixDisabled = false;
-		}, 500);
+		sendToLoop(this, 'sixDisabled');
 	});
 
 	$('#seven').on('motion', function(){
-		if (sevenDisabled) return;
-		sevenDisabled = true;
-		playOnce(this);
-		setTimeout(function() {
-			sevenDisabled = false;
-		}, 350);
+		sendToPlayOnce(this, 'sevenDisabled');
 	});
 
 	$('#eight').on('motion', function(){
-		if (eightDisabled) return;
-		eightDisabled = true;
-		playOnce(this);
-		setTimeout(function() {
-			eightDisabled = false;
-		}, 350);
+		sendToPlayOnce(this, 'eightDisabled');
 	});
 
 	$('#nine').on('motion', function(){
-		if (nineDisabled) return;
-		nineDisabled = true;
-		playOnce(this);
-		setTimeout(function() {
-			nineDisabled = false;
-		}, 350);
+		sendToPlayOnce(this, 'nineDisabled');
 	});
 
 	$('#ten').on('motion', function(){
-		if (tenDisabled) return;
-		tenDisabled = true;
-		playLoop(this, 'ten');
-		setTimeout(function() {
-			tenDisabled = false;
-		}, 500);
+		sendToLoop(this, 'tenDisabled');
 	});
 
 	$('#eleven').on('motion', function(){
-		if (elevenDisabled) return;
-		elevenDisabled = true;
-		playLoop(this, 'eleven');
-		setTimeout(function() {
-			elevenDisabled = false;
-		}, 500);
+		sendToLoop(this, 'elevenDisabled');
 	});
 
 	$('#twelve').on('motion', function(){
-		if (twelveDisabled) return;
-		twelveDisabled = true;
-		playLoop(this, 'twelve');
-		setTimeout(function() {
-			twelveDisabled = false;
-		}, 500);
+		sendToLoop(this, 'twelveDisabled');
 	});
 
 	$('#thirteen').on('motion', function(){
-		if (thirteenDisabled) return;
-		thirteenDisabled = true;
-		playLoop(this, 'thirteen');
-		setTimeout(function() {
-			thirteenDisabled = false;
-		}, 500);
+		sendToLoop(this, 'thirteenDisabled');
 	});
 
 	$('#fourteen').on('motion', function(){
-		if (fourteenDisabled) return;
-		fourteenDisabled = true;
-		playLoop(this, 'fourteen');
-		setTimeout(function() {
-			fourteenDisabled = false;
-		}, 500);
+		sendToLoop(this, 'fourteenDisabled');
 	});
 
 	$('#fifteen').on('motion', function(){
-		if (fifteenDisabled) return;
-		fifteenDisabled = true;
-		playLoop(this, 'fifteen');
-		setTimeout(function() {
-			fifteenDisabled = false;
-		}, 500);
+		sendToLoop(this, 'fifteenDisabled');
 	});
 
 	$('#pause').on('motion', function() {
@@ -251,7 +190,6 @@ function registerListeners(){
 	// increase rate by 0.01 per motion event. Update global variable rate.
 	$('#faster').on('motion', function(){
 		var audios = document.getElementsByClassName('loop');
-		var currentrate;
 		for (var i = 0; i < audios.length; i++) {
 			audios[i].playbackRate += 0.01;
 			rate = audios[i].playbackRate;
@@ -267,5 +205,6 @@ function registerListeners(){
 			console.log('rate', audios[i].playbackRate);
 		}
 	});
-
 }
+
+
